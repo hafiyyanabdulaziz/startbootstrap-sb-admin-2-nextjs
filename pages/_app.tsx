ec589365ccbe44a4d87e94a6ee909780427460a5
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2021-11-09 11:05:22 +0700

    import script, css and create login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,59 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../node_modules/startbootstrap-sb-admin-2/css/sb-admin-2.min.css";
+import "../node_modules/startbootstrap-sb-admin-2/vendor/fontawesome-free/css/all.min.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+        />
+        <meta name='description' content='' />
+        <meta name='author' content='' />
+
+        <title>SB Admin 2 - Login</title>
+
+        {/* <!-- Custom fonts for this template--> */}
+        {/* <link
+          href='vendor/fontawesome-free/css/all.min.css'
+          rel='stylesheet'
+          type='text/css'
+        /> */}
+        <link
+          href='https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i'
+          rel='stylesheet'
+        />
+
+        {/* <!-- Custom styles for this template--> */}
+        {/* <link href='css/sb-admin-2.min.css' rel='stylesheet' /> */}
+      </Head>
+      <Component {...pageProps} />
+
+      {/* <!-- Bootstrap core JavaScript--> */}
+      <Script
+        type='text/javascript'
+        src='/startbootstrap-sb-admin-2/vendor/jquery/jquery.min.js'></Script>
+      <Script
+        type='text/javascript'
+        src='/startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min.js'></Script>
+
+      {/* <!-- Core plugin JavaScript--> */}
+      <Script
+        type='text/javascript'
+        src='/startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min.js'></Script>
+
+      {/* <!-- Custom scripts for all pages--> */}
+      <Script
+        type='text/javascript'
+        src='/startbootstrap-sb-admin-2/js/sb-admin-2.min.js'></Script>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
